4f80fd5c8ebd7b6e9f14023c1a852755d20708c8
Martin De Lojo <martinmdl42@gmail.com>
2024-12-03 03:18:15 -0300

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { PageRouter } from './routes';
+import { NavBar } from './components/NavBar';
 import './App.css';
 
 export default function App() {
   
   return (
     <div className='animatedBackground'>
+      <NavBar />
       <PageRouter />
     </div>
   )
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+// import Grid2 from "@mui/material/Grid2";
+
+
+export function NavBar() {
+
+  return (
+    <>
+    </>
+    // <Grid2 container spacing={0} sx={{backgroundColor:'red'}}>
+    //     <Grid2 size={2} sx={{backgroundColor:'green'}}>
+    //         <p>pic</p>
+    //     </Grid2>
+    //     <Grid2 size={8} sx={{backgroundColor:'white'}}>
+    //         <p>Home</p>
+    //     </Grid2>
+    //     <Grid2 size={8} sx={{backgroundColor:'white'}}>
+    //         <p>Home</p>
+    //     </Grid2>
+    // </Grid2>
+  )
+}
