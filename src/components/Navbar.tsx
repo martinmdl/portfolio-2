cc3d76840c1962158e589b0409b207bc88f72da6
Martin De Lojo <martinmdl42@gmail.com>
2024-12-25 21:11:15 -0300

    finish navbar, TO-DO: active links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,28 @@ import { Link } from "react-router-dom";
 import './Navbar.css';
 
 export function Navbar() {
-  
+
   // const isActive = useMatch(to);
 
   return (
     <>
-      <Link to="/">Work</Link>
-      <Link to="/edu">Education</Link>
-      <Link to="/about">About</Link>
-      <Link to="/resume">Resume</Link>
-      <Link to="/404">404</Link>
+      <header className="header">
+
+        <h1 className="title">
+          Martín De Lojo
+          <p className="typewriter">| Backend Developer</p>          
+        </h1>
+
+        <nav className="navbar">
+          <Link to="/" className="navbar-button active">Work</Link>      
+          <Link to="/edu" className="navbar-button">Education</Link>
+          <Link to="/about" className="navbar-button">About</Link>
+          <Link to="/resume" className="navbar-button">Resume</Link>
+          <Link to="/404" className="navbar-button">404</Link>
+        </nav>
+
+      </header>
+
     </>
   )
 }
