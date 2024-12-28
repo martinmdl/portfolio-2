eb50b66db0ef09a8cda0fc36dbf31be74fc04d2c
Martin De Lojo <martinmdl42@gmail.com>
2024-12-28 02:11:52 -0300

    finish 404 error page

--- src/layouts/00_NotFound.tsx
+++ src/layouts/00_NotFound.tsx
@@ -1,14 +1,14 @@
 import { useNavigate } from 'react-router-dom';
+import './00_NotFound.css';
 
 export function NotFound() {
 
     const navigate = useNavigate();
 
     return (
-        <>
-            <p>404</p>
-
-            <button onClick={() => navigate('/')}>Return</button>
-        </>
+        <main className='main-not-found'>
+            <h1>Error 404: Page not found</h1>
+            <button onClick={() => navigate('/')}>Return to the site</button>
+        </main>
     )
 }
